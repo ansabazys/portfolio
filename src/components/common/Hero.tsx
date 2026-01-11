44b01eecc171aafac7060eae07062d421b8196c8
ansabazys <ansabazys@gmail.com>
2026-01-11 12:57:05 +0530

    feat(menu): add menu toggle

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -3,8 +3,8 @@ export default function Hero() {
     <section className="flex flex-col gap-5 h-full justify-center items-center">
       <div>
         <h1 className="text-3xl w-full max-w-lg font-light text-neutral-400">
-          <span className="text-white">Hey, I'm Ansab.</span> I build, create, and tell stories that make products
-          matter. Based in India, <span className="underline text-white underline-offset-5 decoration-1">Say hello</span>
+          <span className="dark:text-white text-black">Hey, I'm Ansab.</span> I build, create, and tell stories that make products
+          matter. Based in India, <span className="underline dark:text-white text-black underline-offset-5 decoration-1">Say hello</span>
         </h1>
       </div>
 
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,35 @@
+"use client";
+import { useState } from "react";
 import { AnimatedThemeToggler } from "../ui/animated-theme-toggler";
 
 export default function Header() {
+  const [toggle, setToggle] = useState(false);
   return (
-    <header className="p-5 grid grid-cols-3 absolute w-full">
+    <header className="p-5 grid grid-cols-3 fixed w-full">
       <nav className="md:flex hidden gap-10 place-items-start">
         <p>WORK</p>
         <p>ABOUT</p>
         <p>BLOG</p>
       </nav>
 
-      <nav className="md:hidden">
-        <p>MENU</p>
-      </nav>
+      <button
+        className="md:hidden w-fit"
+        onClick={() => setToggle((prev) => !prev)}
+      >
+        MENU
+      </button>
+
+      {toggle && (
+        <section className="absolute p-5 w-full h-screen flex flex-col items-start gap-10 backdrop-blur-lg">
+          <button onClick={() => setToggle((prev) => !prev)}>CLOSE</button>
+          <ul className="text-4xl flex flex-col gap-2 font-semibold tracking-tight">
+            <li>WORK</li>
+            <li>ABOUT</li>
+            <li>BLOG</li>
+            <li>CONTACT</li>
+          </ul>
+        </section>
+      )}
 
       <div className="place-items-center">
         <h1 className="">ANSAB AZYS</h1>
