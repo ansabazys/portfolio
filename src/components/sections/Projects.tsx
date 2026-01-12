28719c31e4857ed798395a3a6a5ea47ee4912355
ansabazys <ansabazys@gmail.com>
2026-01-12 17:16:46 +0530

    fix(project): fix in layout

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,29 +1,9 @@
 export default function Projects() {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-3 ">
-      <div className="w-full flex justify-center items-center h-150 bg-neutral-800 p-5">
-        <div className="w-full max-w-sm flex flex-col gap-2 justify-center items-center h-full">
-          <div className="bg-neutral-900 w-full p-3">
-            <h1 className="font-mono">ECOMMERCE</h1>
-          </div>
-          <div className="flex p-5 bg-neutral-900">
-            <p className="w-full">YEAR: 2026</p>
-
-            <div className="">
-              <p>
-                A high-fashion inspired e-commerce transformation and blending
-                clean...
-              </p>
-            </div>
-          </div>
-          <button className="bg-neutral-900 w-full p-3 font-mono">
-            VIEW PROJECT
-          </button>
-        </div>
-      </div>
-      
-       <div className="w-full flex justify-center items-center h-150 bg-neutral-800 p-5">
-        <div className="w-full max-w-sm flex flex-col gap-2 justify-center items-center h-full">
+    <section className="grid grid-cols-1 gap-5 md:gap-0 lg:grid-cols-3 p-5">
+      <div className="w-full flex flex-col gap-2 h-150 relative  justify-center items-center  ">
+        <div className="h-full bg-neutral-800 md:absolute w-full">a</div>
+        <div className="w-full md:max-w-sm flex z-10 flex-col gap-2 justify-center items-center">
           <div className="bg-neutral-900 w-full p-3">
             <h1 className="font-mono">ECOMMERCE</h1>
           </div>
@@ -43,8 +23,9 @@ export default function Projects() {
         </div>
       </div>
 
-       <div className="w-full flex justify-center items-center h-150 bg-neutral-800 p-5">
-        <div className="w-full max-w-sm flex flex-col gap-2 justify-center items-center h-full">
+      <div className="w-full flex flex-col gap-2 h-150 relative  justify-center items-center  ">
+        <div className="h-full bg-neutral-800 md:absolute w-full">a</div>
+        <div className="w-full md:max-w-sm flex z-10 flex-col gap-2 justify-center items-center">
           <div className="bg-neutral-900 w-full p-3">
             <h1 className="font-mono">ECOMMERCE</h1>
           </div>
@@ -63,6 +44,30 @@ export default function Projects() {
           </button>
         </div>
       </div>
+
+      <div className="w-full flex flex-col gap-2 h-150 relative  justify-center items-center  ">
+        <div className="h-full bg-neutral-800 md:absolute w-full">a</div>
+        <div className="w-full md:max-w-sm flex z-10 flex-col gap-2 justify-center items-center">
+          <div className="bg-neutral-900 w-full p-3">
+            <h1 className="font-mono">ECOMMERCE</h1>
+          </div>
+          <div className="flex p-5 bg-neutral-900">
+            <p className="w-full">YEAR: 2026</p>
+
+            <div className="">
+              <p>
+                A high-fashion inspired e-commerce transformation and blending
+                clean...
+              </p>
+            </div>
+          </div>
+          <button className="bg-neutral-900 w-full p-3 font-mono">
+            VIEW PROJECT
+          </button>
+        </div>
+      </div>
+
+    
     </section>
   );
 }
